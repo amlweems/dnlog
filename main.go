b52684aa648771c5aa9cffeb202e253c4f51a3ab
Anthony Weems <amlweems@gmail.com>
2018-03-08 09:50:35 -0600

    add http server for visibility

--- main.go
+++ main.go
@@ -1,41 +1,50 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
 	"log"
 	"net"
-    "net/http"
+	"net/http"
+	"time"
 
 	"github.com/miekg/dns"
 )
 
-type Log struct{
-    storage []string
-    size int
-    index int
+type Log struct {
+	storage []string
+	size    int
+	index   int
 }
 
 func NewLog(size int) *Log {
-    return &Log{
-        size: size,
-        storage: make([]string, size),
-    }
+	return &Log{
+		size:    size,
+		storage: make([]string, size),
+	}
 }
 
 func (l *Log) Store(v string) {
-    l.storage[l.index % l.size] = v
-    l.index++
+	now := time.Now().UTC().Format("2006/01/02 15:04:05")
+	l.storage[l.index%l.size] = fmt.Sprintf("%s %s", now, v)
+	l.index++
+}
+
+func (l *Log) Reset() {
+	for i := 0; i < l.size; i++ {
+		l.storage[i] = ""
+	}
 }
 
 func (l *Log) Do(do func(string)) {
-    s := l.index
-    for i := 0; i < l.size; i++ {
-        do(l.storage[(s + i) % l.size])
-    }
+	s := l.index
+	for i := 0; i < l.size; i++ {
+		do(l.storage[(s+i)%l.size])
+	}
 }
 
-type server struct{
-    l *Log
+type server struct {
+	l *Log
+	m *http.ServeMux
 }
 
 func (s server) ServeDNS(w dns.ResponseWriter, r *dns.Msg) {
@@ -47,27 +56,41 @@ func (s server) ServeDNS(w dns.ResponseWriter, r *dns.Msg) {
 	}
 
 	for _, question := range r.Question {
-        v := fmt.Sprintf("%16s   %s", addr, question.String())
-        s.l.Store(v)
-        log.Print(v)
+		v := fmt.Sprintf("%16s   %s", addr, question.String())
+		s.l.Store(v)
+		log.Print(v)
 	}
 }
 
 func (s server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-    w.Header().Add("Content-Type", "text/plain")
-    s.l.Do(func(x string) {
-        if x != "" {
-            fmt.Fprintf(w, "%s\n", x)
-        }
-    })
+	w.Header().Add("Content-Type", "text/plain")
+	fmt.Fprint(w, "dns log\n")
+	s.l.Do(func(x string) {
+		if x != "" {
+			fmt.Fprintf(w, "%s\n", x)
+		}
+	})
 }
 
 func main() {
-    s := server{NewLog(128)}
+	s := server{NewLog(128), http.NewServeMux()}
 	go func() {
-        log.Fatal(dns.ListenAndServe(":53", "udp", s))
-    }()
+		log.Fatal(dns.ListenAndServe(":53", "udp", s))
+	}()
+	s.m.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "text/plain")
+		fmt.Fprint(w, "dns log\n")
+		s.l.Do(func(x string) {
+			if x != "" {
+				fmt.Fprintf(w, "%s\n", x)
+			}
+		})
+	})
+	s.m.HandleFunc("/reset", func(w http.ResponseWriter, r *http.Request) {
+		s.l.Reset()
+		w.Header().Add("Content-Type", "text/plain")
+		fmt.Fprint(w, "reset log\n")
+	})
 
-    log.Fatal(http.ListenAndServe(":7000", s))
+	log.Fatal(http.ListenAndServe(":7000", s.m))
 }
-
